df6e460a41bef3ded5c74b7eb937ace180b6142c
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-08-24 14:40:19 +0200

    remove logs

--- script/lang.js
+++ script/lang.js
@@ -1,10 +1,7 @@
-console.log(config);
-
 const textIDs = Object.keys(config.text);
 const language = config.selectedLanguage;
 textIDs.forEach(textID => {
     const elements = document.querySelectorAll(`[text="${textID}"]`);
-    console.log(elements);
     
     elements.forEach(element => {
         element.innerHTML = config.text[textID][language]; 
