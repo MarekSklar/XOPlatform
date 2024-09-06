d9555bf34df50d35d0778873f216fd4a9cd93ab6
Vojtěch Tmej <37409766+vojk@users.noreply.github.com>
2024-09-06 22:56:11 +0200

    Update analytics.js

--- script/analytics.js
+++ script/analytics.js
@@ -7,7 +7,8 @@ async function sendData(data) {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer af312d2b769a633db7092ca1fa1c8235c07f07cd0399d2d3bfa931ac893551244e04801177e3b29f8ff256af264dad2135be75bc76b8a4156402e6d328cfac8e1c3f17d218ca43813b7402d502e7df9ec8cd85bee713de3523968bdc5d253b39b68af9df8842205f771a36650c15d561a8d6b8826cd3484ca8ff3cecc2f75ecf"
+            "Authorization": "Bearer af312d2b769a633db7092ca1fa1c8235c07f07cd0399d2d3bfa931ac893551244e04801177e3b29f8ff256af264dad2135be75bc76b8a4156402e6d328cfac8e1c3f17d218ca43813b7402d502e7df9ec8cd85bee713de3523968bdc5d253b39b68af9df8842205f771a36650c15d561a8d6b8826cd3484ca8ff3cecc2f75ecf",
+            "Access-Control-Allow-Origin": "*"
         },
         body: rawData,
     };
