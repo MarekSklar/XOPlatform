ad84227b7db3fb7220cb09e06133f75651e6c360
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2025-09-25 10:20:16 +0200

    Remove commented proxy middleware code
    Removed proxy configuration for API middleware.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use(express.static(__dirname));
@@ -8,13 +7,6 @@ app.use(express.static(__dirname));
 app.get('/', (_, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
-// Proxy configuration
-// app.use('/api/amos-game-statistics', createProxyMiddleware({
-//   target: 'http://odevzdavani.tourdeapp.cz:1337',  // Your HTTP API
-//   changeOrigin: true,  // Needed for virtual hosted sites
-//   secure: false,       // Ignore SSL certificates (useful for self-signed certificates)
-// }));
 
 const port = 3000;
 app.listen(port, () => {
